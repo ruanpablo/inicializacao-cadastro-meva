579f29c3d3ebc420626acec7c53c9456c70a7d2f
ruanpablo <ruanpablo.santosdejesus1@gmail.com>
2023-04-13 12:38:31 -0300

    Voltando à versão inicial

--- src/Usuario.java
+++
@@ -1,7 +0,0 @@
-public class Usuario {
-    public static void main(String[] args) {
-        System.out.println("Switch");
-        System.out.println("commit");
-        System.out.println("branch");
-    }
-}
--- src/cliente.java
+++
@@ -1,5 +0,0 @@
-public class cliente {
-    public static void main(String[] args) {
-        System.out.println("Olá!");
-    }
-}
